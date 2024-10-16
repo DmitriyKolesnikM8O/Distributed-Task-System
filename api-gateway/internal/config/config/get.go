d439b63a8f55fca71eeaa5bcf60c165c772ac89d
Dmitriy <kolesnik982011@gmail.com>
2024-10-16 17:30:19 +0300

    changes functions for read config files to viper

--- api-gateway/internal/config/config/get.go
+++ api-gateway/internal/config/config/get.go
@@ -2,24 +2,43 @@ package config
 
 import (
 	"log"
-	"sync"
 
-	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/spf13/viper"
 )
 
 var (
 	instance *Config
-	one      sync.Once
+	// one      sync.Once
 )
 
+//Old version function for read config
+// func GetConfig() *Config {
+// 	one.Do(func() {
+// 		log.Printf("Read config")
+// 		instance = &Config{}
+// 		err := cleanenv.ReadConfig("internal/config/config.yml", instance)
+// 		if err != nil {
+// 			log.Fatalf("Config error: %s", err)
+// 		}
+// 	})
+// 	return instance
+// }
+
 func GetConfig() *Config {
-	one.Do(func() {
-		log.Printf("Read config")
-		instance = &Config{}
-		err := cleanenv.ReadConfig("internal/config/config.yml", instance)
-		if err != nil {
+	log.Printf("Read config")
+	v := viper.New()
+	v.AddConfigPath("internal/config/")
+	v.SetConfigName("config")
+	v.SetConfigType("yml")
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Fatalf("Config file not found: %s", err)
+		} else {
 			log.Fatalf("Config error: %s", err)
 		}
-	})
+	}
+
+	v.Unmarshal(&instance)
 	return instance
 }
